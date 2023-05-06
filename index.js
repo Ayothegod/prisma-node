b5bb4f001d9640b292ff144e71a564f5ea17a9d8
Ayo <heyayomideadebisi@gmail.com>
2023-05-06 17:59:31 +0100

    add prisma and prisma client

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = 3001
 
 app.get("/",(req,res) => {
-    res.status(200).json("hello")
+    res.status(200).json({"hello":"hii"})
 })
 
-app.listen(PORT,() => console.log(`server running on port ${PORT}`))
+app.listen(PORT,() => console.log(`server is running on port ${PORT}`))
