bcfdbc15bb61ddc0bcc33f62956e8055ab4712b5
Ayo <heyayomideadebisi@gmail.com>
2023-05-08 21:51:15 +0100

    create post request

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const app = express()
 const PORT = 3001
 const { PrismaClient } = require('@prisma/client')
 // import  from '@prisma/client'
-
 const prisma = new PrismaClient()
 
-app.get("/",(req,res) => {
+app.use(express.json())
+
+app.get("/", (req, res) => {
     const allUsers = prisma.user.findMany()
     res.json(allUsers)
 })
 
-app.listen(PORT,() => console.log(`server is running on port ${PORT}`))
+app.post("/", async (req, res) => {
+    const newUser = await prisma.user.create({ data: req.body })
+    res.json(newUser)
+})
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
