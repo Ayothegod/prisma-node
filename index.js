7ddc9d549a177fd7ea4b5c6d467361c4d5c080be
Ayo <heyayomideadebisi@gmail.com>
2023-05-08 21:29:24 +0100

    start dev on index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const app = express()
 const PORT = 3001
+// import { PrismaClient } from '@prisma/client'
+// const prisma = new PrismaClient()
 
 app.get("/",(req,res) => {
-    res.status(200).json({"hello":"hii"})
+    res.status(200).json("Hello")
 })
 
 app.listen(PORT,() => console.log(`server is running on port ${PORT}`))
